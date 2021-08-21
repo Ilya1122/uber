25e8565f9273378bf7c6e7ca1fd6760f4a1eafa1
i.rudiuk1997.11.22 <i.rudiuk1997.11.22@gmail.com>
2021-08-21 05:01:54 +0300

    added navigations, google map

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {FlatList, TouchableOpacity, View, Image, Text} from "react-native";
 import tw from 'tailwind-react-native-classnames'
+import { Icon } from "react-native-elements";
+import {useNavigation} from "@react-navigation/native";
+import {useSelector} from "react-redux";
+import {selectOrigin} from "../slices/navSlice";
+import {opacity} from "react-native-reanimated/src/reanimated2/Colors";
 
 const data = [
     {
@@ -18,19 +23,29 @@ const data = [
 ]
 
 const NavOptions = () => {
+    const navigation = useNavigation()
+    const origin = useSelector(selectOrigin)
     return (
         <FlatList
             data={data}
             horizontal
             keyExtractor={({ id }) => id}
             renderItem={({ item }) => (
-                <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                    <View>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                    disabled={!origin}
+                >
+                    <View style={tw`${!origin && 'opacity-20'}`}>
                         <Image
                             style={{ width: 120, height: 120, resizeMode: 'contain' }}
                             source={{ uri: item.image }}
                         />
                         <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                        <Icon
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                            name='arrowright' color='white' type='antdesign'
+                        />
                     </View>
                 </TouchableOpacity>
             )}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {StyleSheet, Text, View, SafeAreaView, Image} from "react-native";
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from "../components/NavOptions";
+import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import {useDispatch} from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch()
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
            <View style={tw`p-5`}>
@@ -16,6 +21,34 @@ const HomeScreen = () => {
                    source={{
                        uri: 'https://links.papareact.com/gzs',
                    }}
+               />
+               <GooglePlacesAutocomplete
+                   styles={{
+                       container: {
+                           flex: 0,
+                       },
+                       textInput: {
+                           fontSize: 18,
+                       }
+                   }}
+                   onPress={(data, details= null) => {
+                       dispatch(setOrigin({
+                           location: details.geometry.location,
+                           description: data.description,
+                       }))
+                       dispatch(setDestination(null))
+                   }}
+                   minLength={3}
+                   fetchDetails={true}
+                   returnKeyType={"search"}
+                   enablePoweredByContainer={false}
+                   query={{
+                       key: GOOGLE_MAPS_APIKEY,
+                       language: 'en'
+                   }}
+                   placeholder='Where From?'
+                   debounce={400}
+                   nearbyPlacesAPI='GooglePlacesSearch'
                />
                <NavOptions />
            </View>
