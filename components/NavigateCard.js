e16692d820c7c2348b9685370e8bbe8dbb6259b2
i.rudiuk1997.11.22 <i.rudiuk1997.11.22@gmail.com>
2021-08-21 05:36:50 +0300

    added navigations, google map

---
+++ components/NavigateCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {FlatList, TouchableOpacity, View, Image, Text} from "react-native";
+
+const NavigateCard = () => {
+    return (
+        <View>
+            <Text>NavigateCard</Text>
+        </View>
+    );
+};
+
+export default NavigateCard;
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const RideOptionsCard = () => {
+    return (
+        <View>
+            <Text>
+                RideOptionsCard
+            </Text>
+        </View>
+    );
+};
+
+export default RideOptionsCard;
